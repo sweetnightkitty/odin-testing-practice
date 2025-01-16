6ebddb502a06ddfac2d2d74485ffcb6abd62e558
SaraToth <saratoth101@gmail.com>
2025-01-17 08:55:33 +0900

    Test: Write test for captialize function
    Write unit tests that confirm all features of capitalization function perform as expected

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,7 +1,24 @@
 import { capitalize } from "./capitalize";
 
 describe ("Capitalize function", () => {
-    it("capitalize function exists", () => {
-        expect(capitalize()).toBeDefined;
+    it("capitalize the first letter", () => {
+        expect(capitalize("word")).toBe("Word");
+        expect(capitalize("sara")).toBe("Sara");
+        expect(capitalize("something")).toBe("Something");
     })
+
+    it("Capitalize first letter in a sentence", () => {
+        expect(capitalize("this is a sentence.")).toBe("This is a sentence.");
+        expect(capitalize("another sentence?")).toBe("Another sentence?");
+    })
+
+    it("Capitalize an empty string returns an empty string", () => {
+        expect(capitalize("")).toBe("");
+    })
+
+    it("Capitalize does not effect already capitalized words/sentence", ()=> {
+        expect(capitalize("This is done.")).toBe("This is done.");
+        expect(capitalize("My name is Sara.")).toBe("My name is Sara.");
+    })
+
 })
