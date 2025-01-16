62c42e832909bd968f6e2f19a273b1dea957c531
SaraToth <saratoth101@gmail.com>
2025-01-17 08:54:41 +0900

    Feat: Capitalize first letter in a string
    Write a function that capitalizes the first letter in a string whether its a word or a sentence

--- capitalize.js
+++ capitalize.js
@@ -1,2 +1,5 @@
-export function capitalize() {
+export function capitalize(word) {
+    const firstLetter = String(word).charAt(0).toUpperCase();
+    const remainingLetters = word.slice(1);
+    return firstLetter + remainingLetters;
 };
