084c6faf3c277b8692374e4dca3b9ac944a3086b
SaraToth <saratoth101@gmail.com>
2025-01-20 08:25:21 +0900

    Test: Write Jest test for caesarCipher
    Confirm that the caesarCipher function works so far

---
+++ caesarCipher.test.js
@@ -0,0 +1,19 @@
+import { callExpression } from "@babel/types";
+import { casearCipher } from "./caesarCipher";
+
+describe("Caesar Cipher", ()=>{
+    it("Throws error if no parameter entered", ()=> {
+        expect(()=> casearCipher()).toThrow();
+    })
+
+    it("Returns a shifted word", ()=> {
+        expect(casearCipher("dog")).toBe("grj");
+        expect(casearCipher("cat")).toBe("fdw");
+        expect(casearCipher("hello")).toBe("khoor");
+    })
+
+    it("Throws error if parameter provided is not a string", ()=> {
+        expect(()=> casearCipher(3)).toThrow();
+    })
+
+})
