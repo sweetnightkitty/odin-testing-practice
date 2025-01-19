102aed210c8a275071ad4227e51055a0044aec76
SaraToth <saratoth101@gmail.com>
2025-01-20 08:30:55 +0900

    Test: Jest test confirming shiftValue works
    Create test to confirm that shiftValue can be implemented properly but that the cipher will work without a shiftValue - with a default set to 3

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -6,7 +6,7 @@ describe("Caesar Cipher", ()=>{
         expect(()=> casearCipher()).toThrow();
     })
 
-    it("Returns a shifted word", ()=> {
+    it("Returns a shifted word by default shiftValue 3", ()=> {
         expect(casearCipher("dog")).toBe("grj");
         expect(casearCipher("cat")).toBe("fdw");
         expect(casearCipher("hello")).toBe("khoor");
@@ -14,6 +14,11 @@ describe("Caesar Cipher", ()=>{
 
     it("Throws error if parameter provided is not a string", ()=> {
         expect(()=> casearCipher(3)).toThrow();
+    })
+
+    it("Creates a new code of words shifted by any value", ()=> {
+        expect(casearCipher("dog", 3)).toBe("grj");
+        expect(casearCipher("dog", 5)).toBe("itl");
     })
 
 })
